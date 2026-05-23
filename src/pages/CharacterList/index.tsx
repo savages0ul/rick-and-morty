062adc62b58723f709c66cc0cfd8a6d5cb82e2a7
savages0ul <kashin071@mail.ru>
2026-05-23 16:13:38 +0300

    Добавлен компонент текстового поля

--- src/pages/CharacterList/index.tsx
+++ src/pages/CharacterList/index.tsx
@@ -7,16 +7,15 @@ import {
   SPECIES_OPTIONS,
   STATUS_OPTIONS
 } from '../../components/Select/mocks';
+import { TextField } from '../../components/TextField';
 import styles from './styles.module.scss';
 
 export const CharacterList = () => {
+  const [name, setName] = useState('');
+  const [formName, setFormName] = useState('');
   const [species, setSpecies] = useState<string>();
   const [gender, setGender] = useState<string>();
   const [status, setStatus] = useState<string>();
-
-  const [speciesSmall, setSpeciesSmall] = useState<string>();
-  const [genderSmall, setGenderSmall] = useState<string>();
-  const [statusSmall, setStatusSmall] = useState<string>();
 
   return (
     <div className={styles.page}>
@@ -28,6 +27,12 @@ export const CharacterList = () => {
         />
       </div>
       <div className={styles.filters}>
+        <TextField
+          variant='bordered'
+          value={name}
+          onChange={setName}
+          placeholder='Filter by name...'
+        />
         <Select
           placeholder='Species'
           options={SPECIES_OPTIONS}
@@ -48,27 +53,12 @@ export const CharacterList = () => {
         />
       </div>
       {/* TODO: для теста */}
-      <div className={styles.filtersSmall}>
-        <Select
-          size='small'
-          placeholder='Species'
-          options={SPECIES_OPTIONS}
-          value={speciesSmall}
-          onChange={setSpeciesSmall}
-        />
-        <Select
-          size='small'
-          placeholder='Gender'
-          options={GENDER_OPTIONS}
-          value={genderSmall}
-          onChange={setGenderSmall}
-        />
-        <Select
-          size='small'
-          placeholder='Status'
-          options={STATUS_OPTIONS}
-          value={statusSmall}
-          onChange={setStatusSmall}
+      <div className={styles.formTest}>
+        <TextField
+          variant='underlined'
+          value={formName}
+          onChange={setFormName}
+          placeholder='Rick Sanchez'
         />
       </div>
     </div>
